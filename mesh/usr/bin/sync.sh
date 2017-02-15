cded5d9fa1a61abe1973c1852ac2480fc918f338
blueskymana <1024791744@qq.com>
2017-02-15 17:29:04 +0800

    update configuration files.

--- mesh/usr/bin/sync.sh
+++ mesh/usr/bin/sync.sh
@@ -11,26 +11,41 @@ BRCTL=/usr/sbin/brctl
 IFCONFIG=/sbin/ifconfig
 
 /etc/init.d/mesh stop
-$MP add $mesh_iface $wlan nexfi-sync $channel
-$IFCONFIG $mesh_iface 172.2.2.1 netmask 255.255.255.0
+orig_ssid=$(uci get wireless.@wifi-iface[1].ssid)
+orig_bssid=$(uci get wireless.@wifi-iface[1].bssid)
+
+uci set wireless.@wifi-iface[1].ssid=$ssid
+uci set wireless.@wifi-iface[1].mode=$bssid
+uci commit wireless
+/etc/init.d/network reload
+sleep 3
+$IFCONFIG $wlan 172.16.73.1 netmask 255.255.255.0
 
 # sync data
 ssid=$(uci get wireless.@wifi-iface[0].ssid)
 key=$(uci get wireless.@wifi-iface[0].key)
 encryption=$(uci get wireless.@wifi-iface[0].encryption)
 ssid2=$(uci get wireless.@wifi-iface[1].ssid)
-syncdata="ssid:${ssid}:key:${key}:encryption:${encryption}:ssid2:${ssid2}"
+bssid=$(uci get wireless.@wifi-iface[1].bssid)
+syncdata="ssid:${ssid}:key:${key}:encryption:${encryption}:ssid2:${ssid2}:bssid:${bssid}"
 
-bcast=172.2.2.255
+bcast=172.16.73.255
 # transfer data
 
-for i in {1 2 3 4 5 6 7 8}
+for i in {1 2 3 4 5 6 7 8 9 10}
 do
     udpecho $bcast $syncdata 
     echo $syncdata
     sleep 1
 done
 
-/etc/init.d/mesh stop
+uci set wireless.@wifi-iface[1].ssid=$orig_ssid
+uci set wireless.@wifi-iface[1].mode=$orig_bssid
+uci commit wireless
+
+/etc/init.d/network reload
+sleep 3
 /etc/init.d/mesh restart
 
+#$MP add $mesh_iface $wlan nexfi-sync $channel
+#$IFCONFIG $mesh_iface 172.16.73.1 netmask 255.255.255.0
--- mesh/usr/bin/syncr.sh
+++ mesh/usr/bin/syncr.sh
@@ -8,6 +8,7 @@ channel=$(uci get mesh.@mesh-iface[0].channel)
 mesh_iface=$(uci get mesh.@mesh-iface[0].mesh_iface)
 wlan=$(uci get mesh.@mesh-iface[0].wlan)
 ssid=$(uci get mesh.@mesh-iface[0].ssid)
+bssid=$(uci get mesh.@mesh-iface[0].bssid)
 bridge=$(uci get mesh.@mesh-iface[0].bridge)
 
 MP=/usr/bin/mp
@@ -15,23 +16,33 @@ BRCTL=/usr/sbin/brctl
 IFCONFIG=/sbin/ifconfig
 
 /etc/init.d/mesh stop
-$MP add $mesh_iface $wlan nexfi-sync $channel
-$IFCONFIG $mesh_iface 172.2.2.2 netmask 255.255.255.0
+uci set wireless.@wifi-iface[1].ssid=$ssid
+uci set wireless.@wifi-iface[1].mode=$bssid
+uci commit wireless
+/etc/init.d/network reload
 
+sleep 3
+$IFCONFIG $WAN 172.16.73.2 netmask 255.255.255.0
 netconfig=$(udpecho -s)
 
 ssid=$(get_option $netconfig ssid)
 key=$(get_option $netconfig key)
 encryption=$(get_option $netconfig encryption)
 ssid2=$(get_option $netconfig ssid2)
+bssid=$(get_option $netconfig bssid)
 
 uci set wireless.@wifi-iface[0].ssid=$ssid
 uci set wireless.@wifi-iface[0].key=$key
 uci set wireless.@wifi-iface[0].encryption=$encryption
 
 uci set wireless.@wifi-iface[1].ssid=$ssid2
+uci set wireless.@wifi-iface[1].bssid=$bssid
 uci commit wireless
 
 /etc/init.d/network restart
 sleep 3
 /etc/init.d/mesh restart
+
+#/etc/init.d/mesh stop
+#$MP add $mesh_iface $wlan nexfi-sync $channel
+#$IFCONFIG $mesh_iface 172.16.73.2 netmask 255.255.255.0
