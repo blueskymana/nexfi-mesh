367e51cd7aecd6284f930c67e3b4e2f5eb097f07
blueskymana <1024791744@qq.com>
2017-02-17 17:20:55 +0800

    fixed ebtables bug.

--- mesh/usr/bin/ebtables.sh
+++ mesh/usr/bin/ebtables.sh
@@ -6,5 +6,4 @@ ebtables -P FORWARD ACCEPT
 ebtables -P INPUT ACCEPT
 ebtables -P OUTPUT ACCEPT
 ebtables -F 
-ebtables -A FORWARD -p ipv4 -i eth0 --ip-dst 173.173.173.1 -j DROP
-ebtables -A FORWARD -p ipv4 -i eth0 --ip-dst 173.173.173.2 -j DROP
+ebtables -A FORWARD -p ipv4 -i eth0 --ip-dst 172.16.16.16 -j DROP
