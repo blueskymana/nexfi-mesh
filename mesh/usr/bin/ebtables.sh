7399efeefb75794b71df729ccf5909636436b0b5
blueskymana <1024791744@qq.com>
2017-02-28 18:02:21 +0800

    fixed mesh point.

--- mesh/usr/bin/ebtables.sh
+++ mesh/usr/bin/ebtables.sh
@@ -6,4 +6,4 @@ ebtables -P FORWARD ACCEPT
 ebtables -P INPUT ACCEPT
 ebtables -P OUTPUT ACCEPT
 ebtables -F 
-ebtables -A FORWARD -p ipv4 -i eth0 --ip-dst 172.16.16.16 -j DROP
+ebtables -A FORWARD -p ipv4 -i eth0 --ip-dst 192.168.8.8 -j DROP
